3ac51d45167e7da7f46b5f20bc7a5c28b5807ccf
Orsolya Szocs <orsolya.szocs@mhp.com>
2020-08-10 13:09:24 +0300

    user and manager paths secured

--- webapp/controller/Manager.controller.js
+++ webapp/controller/Manager.controller.js
@@ -1,108 +1,150 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "intern2020/controller/BaseController",
-    'sap/ui/model/Filter',
-    'sap/ui/model/FilterOperator',
-    'sap/ui/model/Sorter',
-    'sap/ui/model/json/JSONModel',
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
-    'sap/m/MessageToast'
-], function(BaseController, Filter, FilterOperator, Sorter, JSONModel, ResourceModel, MessageToast) {
-"use strict";
+    "sap/m/MessageToast",
+  ],
+  function (
+    BaseController,
+    Filter,
+    FilterOperator,
+    Sorter,
+    JSONModel,
+    ResourceModel,
+    MessageToast
+  ) {
+    "use strict";
 
-return BaseController.extend("intern2020.controller.Manager", {
-
-    
-    onInit : function (oContext) {
-        
+    return BaseController.extend("intern2020.controller.Manager", {
+      onInit: function (oContext) {
         //var UserId = oContext.getProperty("UId");
         //if (typeof(Storage) !== "undefined") {
-            // Store
-            //localStorage.setItem("lastname", UserId);
-            // Retrieve
-           // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-          //} else {
-           // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-          //}
-      
-     },
+        // Store
+        //localStorage.setItem("lastname", UserId);
+        // Retrieve
+        // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+        //} else {
+        // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+        //}
 
-     getRouter: function () {
-            
+        this.getOwnerComponent()
+          .getRouter()
+          .getRoute("manager")
+          .attachPatternMatched(this.checkLogin, this);
+      },
+
+      checkLogin: function () {
+        var userInfo = this.getOwnerComponent().getModel("UserInfo").getData();
+        if (!userInfo.isManager) {
+          jQuery.sap.require("sap.m.MessageBox");
+          var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+          console.log("error message");
+          sap.m.MessageBox.error(
+            "You must be logged in if you want to use the application.",
+            {
+              title: "Log in",
+              onClose: function () {
+                oRouter.navTo("login");
+              },
+              styleClass: "", // default
+              actions: sap.m.MessageBox.Action.Close, // default
+              emphasizedAction: null, // default
+              initialFocus: null, // default
+              textDirection: sap.ui.core.TextDirection.Inherit, // default
+            }
+          );
+        }
+      },
+
+      getRouter: function () {
         return sap.ui.core.UIComponent.getRouterFor(this);
-    },
+      },
 
-    onSliderMoved: function (oEvent) {
+      onSliderMoved: function (oEvent) {
         var iValue = oEvent.getParameter("value");
         this.byId("otbSubheader").setWidth(iValue + "%");
         this.byId("otbFooter").setWidth(iValue + "%");
-    },
+      },
 
-    _fnGroup : function (oContext){
+      _fnGroup: function (oContext) {
         var UserId = oContext.getProperty("UId");
 
         return {
-            key : UserId,
-            text : UserId
+          key: UserId,
+          text: UserId,
         };
-    },
+      },
 
-    onGoToLogin : function(oEvent){
-        
+      onGoToLogin: function (oEvent) {
+        var data = { isUser: false, isManager: false };
+        var oModel = new JSONModel(data);
+        var userInfo = this.getOwnerComponent().setModel(oModel, "UserInfo");
+
         var oRouter = this.getRouter();
         oRouter.navTo("login");
+      },
 
-    },
-
-    onReset: function (oEvent){
+      onReset: function (oEvent) {
         this.bGrouped = false;
         this.bDescending = false;
         this.sSearchQuery = 0;
         this.byId("UId").setValue("");
         this.fnApplyFiltersAndOrdering();
-    },
+      },
 
-    onGroup: function (oEvent){
+      onGroup: function (oEvent) {
         this.bGrouped = !this.bGrouped;
         this.fnApplyFiltersAndOrdering();
-    },
+      },
 
-    onSort: function (oEvent) {
+      onSort: function (oEvent) {
         this.bDescending = !this.bDescending;
         this.fnApplyFiltersAndOrdering();
-    },
+      },
 
-    onFilter: function (oEvent) {
+      onFilter: function (oEvent) {
         this.sSearchQuery = oEvent.getSource().getValue();
         this.fnApplyFiltersAndOrdering();
-    },
+      },
 
-    onTogglePress: function(oEvent) {
+      onTogglePress: function (oEvent) {
         var oButton = oEvent.getSource(),
-            bPressedState = oButton.getPressed(),
-            sStateToDisplay = bPressedState ? "Pressed" : "Unpressed";
+          bPressedState = oButton.getPressed(),
+          sStateToDisplay = bPressedState ? "Pressed" : "Unpressed";
 
         MessageToast.show(oButton.getId() + " " + sStateToDisplay);
-    },
+      },
 
-    fnApplyFiltersAndOrdering: function (oEvent){
+      fnApplyFiltersAndOrdering: function (oEvent) {
         var aFilters = [],
-            aSorters = [];
+          aSorters = [];
 
         if (this.bGrouped) {
-            aSorters.push(new Sorter("UId", this.bDescending, this._fnGroup));
+          aSorters.push(new Sorter("UId", this.bDescending, this._fnGroup));
         } else {
-            aSorters.push(new Sorter("RId", this.bDescending));
+          aSorters.push(new Sorter("RId", this.bDescending));
         }
 
         if (this.sSearchQuery) {
-            var oFilter = new Filter("RId", FilterOperator.Contains, this.sSearchQuery);
-            aFilters.push(oFilter);
+          var oFilter = new Filter(
+            "RId",
+            FilterOperator.Contains,
+            this.sSearchQuery
+          );
+          aFilters.push(oFilter);
         }
 
-        this.byId("idProductsTable").getBinding("items").filter(aFilters).sort(aSorters);
-    }
-});
+        this.byId("idProductsTable")
+          .getBinding("items")
+          .filter(aFilters)
+          .sort(aSorters);
+      },
+    });
 
-return OverflowToolbarController;
-
-});
+    return OverflowToolbarController;
+  }
+);
