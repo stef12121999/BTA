358e27f6c8add24b628a7f859071cb8e9fe14ed2
Teodora Nechita <teodora.nechita@mhp.com>
2020-08-10 10:08:58 +0300

    mesaj

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,8 +1,72 @@
 sap.ui.define([
-    "intern2020/BaseController"
- ], function (BaseController) {
+    "intern2020/controller/BaseController",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/Fragment"
+ ], function (BaseController, MessageToast,JSONModel,ResourceModel, Fragment) {
     "use strict";
     return BaseController.extend("intern2020.controller.Login", {
+  
+        onInit : function (oEvent) {
+             
+        },
+        getRouter: function () {
+            
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+        
+        onValidation: function(oEvent) {
+  
+            var oView = this.getView();
+            var oModel = oView.getModel();
+            var UserId = oView.byId("username").getValue();
+            var UserPassword = oView.byId("password").getValue();
+  
+            if (UserId == "") {
+                MessageToast.show("Please Enter Username");
+                return false;
+            } else if (UserPassword == "") {
+                MessageToast.show("Please Enter Password");
+                return false;
+            } else {
+               //var sPath = oModel.createKey("/EmployeeSet", {
+                //Id : UserId,
+                //Password : UserPassword
+                  //});
+  
+                oModel.read("/EmployeeSet(Id='"+UserId+"',Password='"+UserPassword+"')", 
+     
+            {
+                success : function(oData){
+                    
+                    
+                    if(oData.IsManager == 1)
+                        {
+                            var oRouter = this.getRouter();
+                            oRouter.navTo("manager");
+                            MessageToast.show("Success manager");
+                            
+                        }
+                   else
+                   {
+                        var oRouter = this.getRouter();
+                        oRouter.navTo("user");
+                        MessageToast.show("Success user");
+                   }
+                        
+  
+                }.bind(this),
+
+                error : function(){
+                    MessageToast.show("Login Failed");
+  
+                }
+            });
+                
+                
+            }
+        }
        
     });
  });
---
+++ webapp/controller/Manager.controller.js
@@ -0,0 +1,108 @@
+sap.ui.define([
+    "intern2020/controller/BaseController",
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/ui/model/Sorter',
+    'sap/ui/model/json/JSONModel',
+    "sap/ui/model/resource/ResourceModel",
+    'sap/m/MessageToast'
+], function(BaseController, Filter, FilterOperator, Sorter, JSONModel, ResourceModel, MessageToast) {
+"use strict";
+
+return BaseController.extend("intern2020.controller.Manager", {
+
+    
+    onInit : function (oContext) {
+        
+        //var UserId = oContext.getProperty("UId");
+        //if (typeof(Storage) !== "undefined") {
+            // Store
+            //localStorage.setItem("lastname", UserId);
+            // Retrieve
+           // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+          //} else {
+           // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+          //}
+      
+     },
+
+     getRouter: function () {
+            
+        return sap.ui.core.UIComponent.getRouterFor(this);
+    },
+
+    onSliderMoved: function (oEvent) {
+        var iValue = oEvent.getParameter("value");
+        this.byId("otbSubheader").setWidth(iValue + "%");
+        this.byId("otbFooter").setWidth(iValue + "%");
+    },
+
+    _fnGroup : function (oContext){
+        var UserId = oContext.getProperty("UId");
+
+        return {
+            key : UserId,
+            text : UserId
+        };
+    },
+
+    onGoToLogin : function(oEvent){
+        
+        var oRouter = this.getRouter();
+        oRouter.navTo("login");
+
+    },
+
+    onReset: function (oEvent){
+        this.bGrouped = false;
+        this.bDescending = false;
+        this.sSearchQuery = 0;
+        this.byId("UId").setValue("");
+        this.fnApplyFiltersAndOrdering();
+    },
+
+    onGroup: function (oEvent){
+        this.bGrouped = !this.bGrouped;
+        this.fnApplyFiltersAndOrdering();
+    },
+
+    onSort: function (oEvent) {
+        this.bDescending = !this.bDescending;
+        this.fnApplyFiltersAndOrdering();
+    },
+
+    onFilter: function (oEvent) {
+        this.sSearchQuery = oEvent.getSource().getValue();
+        this.fnApplyFiltersAndOrdering();
+    },
+
+    onTogglePress: function(oEvent) {
+        var oButton = oEvent.getSource(),
+            bPressedState = oButton.getPressed(),
+            sStateToDisplay = bPressedState ? "Pressed" : "Unpressed";
+
+        MessageToast.show(oButton.getId() + " " + sStateToDisplay);
+    },
+
+    fnApplyFiltersAndOrdering: function (oEvent){
+        var aFilters = [],
+            aSorters = [];
+
+        if (this.bGrouped) {
+            aSorters.push(new Sorter("UId", this.bDescending, this._fnGroup));
+        } else {
+            aSorters.push(new Sorter("RId", this.bDescending));
+        }
+
+        if (this.sSearchQuery) {
+            var oFilter = new Filter("RId", FilterOperator.Contains, this.sSearchQuery);
+            aFilters.push(oFilter);
+        }
+
+        this.byId("idProductsTable").getBinding("items").filter(aFilters).sort(aSorters);
+    }
+});
+
+return OverflowToolbarController;
+
+});
