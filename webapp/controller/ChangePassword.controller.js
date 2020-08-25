dd245520c9f490416fb90aba22c73451190bf8d3
Orsolya Szocs <orsolya.szocs@mhp.com>
2020-08-25 09:25:27 +0300

    change password done

---
+++ webapp/controller/ChangePassword.controller.js
@@ -0,0 +1,88 @@
+sap.ui.define(
+  [
+    "intern2020/controller/BaseController",
+    "sap/m/MessageToast",
+    "sap/m/Button",
+    "sap/m/Dialog",
+    "sap/m/Label",
+    "sap/m/Text",
+    "sap/m/TextArea",
+    "sap/ui/core/routing/History",
+  ],
+  function (
+    BaseController,
+    MessageToast,
+    Button,
+    Dialog,
+    Label,
+    Text,
+    TextArea,
+    History
+  ) {
+    "use strict";
+    return BaseController.extend("intern2020.controller.ChangePassword", {
+      onInit: function (oEvent) {
+        this.getRouter()
+          .getRoute("changePassword")
+          .attachPatternMatched(this.patternMatched, this);
+        this.checkLoginUser();
+      },
+
+      onNavBack: function () {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = this.getRouter();
+          oRouter.navTo("user", true);
+        }
+      },
+
+      onChangePassword: function () {
+        var oModel = this.getView().getModel();
+        var username = this.getUsername();
+        var oView = this.getView();
+        var oldPass = oView.byId("inputOld").getValue();
+        var newPass = oView.byId("inputNew").getValue();
+        oModel.update(
+          "/EmployeeSet(Id='" + username + "',Password='" + oldPass + "')",
+          { Password: newPass },
+          {
+            success: function () {
+              MessageToast.show("Your password was changed.");
+            },
+            error: function () {
+              MessageToast.show("Old password is incorrect.");
+            },
+          }
+        );
+      },
+
+      patternMatched: function (oEvent) {
+        this.checkLoginUser();
+      },
+
+      //     onSendEmail: function (oEvent) {
+
+      //       var oView = this.getView();
+      //       var oModel = oView.getModel();
+      //       var UserId = oView.byId("input-c").getValue();
+
+      //       oModel.callFunction("/Change_Password", {
+      //         method: "POST",
+      //         urlParameters: {
+      //           UserId: UserId,
+      //         },
+      //         success: function (oData) {
+      //           MessageToast.show("Your new password will be sent to you in an email.");
+      //         }.bind(this),
+      //         error: function () {
+      //           MessageToast.show("Are you sure that this is your correct email address?");
+      //         },
+      //       });
+      //     },
+    });
+  }
+);
