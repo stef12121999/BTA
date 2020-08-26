3008f5f58e090dea08f2a5378a4d0338ce80cfba
Orsolya Szocs <orsolya.szocs@mhp.com>
2020-08-26 17:32:12 +0300

    deletions done

--- internProj/webapp/controller/App.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-   "intern2020/controller/BaseController"
-], function (BaseController) {
-   "use strict";
-   return BaseController.extend("intern2020.controller.App", {
-      
-   });
-});
